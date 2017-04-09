5fa119baf8a8deb09a457f105a402cf5a74bedee
bommanaraviteja <bommana.raviteja@gmail.com>
2017-04-09 15:23:19 +0530

    Test Rest Call

---
+++ TestRestCall/src/main/java/com/example/TestRestCallApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRestCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRestCallApplication.class, args);
+	}
+}
